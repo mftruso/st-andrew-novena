108fcebcaee276a59482c9b580a00083425b552a
Mike Truso <mike@agileorbit.com>
2019-10-27 13:38:28 -0500

    persist notification settings

--- lib/settingsPage.dart
+++ lib/settingsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key}) : super(key: key);
@@ -10,26 +11,54 @@ class SettingsPage extends StatefulWidget {
 class _SettingPagesState extends State<SettingsPage> {
   bool _notificationsEnabled = false;
 
+  Future _initializeSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool notificationsEnabled = prefs.getBool('notifications_enabled');
+    setState(() {
+      _notificationsEnabled = notificationsEnabled ?? false;
+    });
+  }
+
+  Future _storeNotificationPreferences(value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('notifications_enabled', value);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeSettings();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text('Settings'),
-            automaticallyImplyLeading: true,
-            leading: IconButton(icon:Icon(Icons.arrow_back),
-              onPressed:() => Navigator.pop(context, false),
-            ),
+          title: Text('Settings'),
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context, false),
+          ),
         ),
         body: Center(
             child: Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Notification Preferences',
-                        style: Theme.of(context).textTheme.display1,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('Notifications'),
+                      trailing: Switch(
+                        value: _notificationsEnabled,
+                        onChanged: (value) {
+                          setState(() {
+                            _notificationsEnabled = value;
+                            _storeNotificationPreferences(value);
+                          });
+                        },
                       ),
-                    ]))));
+                    )
+                  ],
+                ))));
   }
 }
